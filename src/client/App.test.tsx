2d6fd8401dc30b364ebf9d0dcd668ae4a1e9e103
raosan <raosanf.lillahi@gmail.com>
2021-05-27 10:18:45 +0700

    fix: test

--- src/client/App.test.tsx
+++ src/client/App.test.tsx
@@ -23,5 +23,5 @@ import App from "./App";
 
 it("renders welcome message", () => {
   render(<App />);
-  expect(screen.getByText("Hello Symon")).toBeInTheDocument();
+  expect(screen.getByText("Create new user")).toBeInTheDocument();
 });
